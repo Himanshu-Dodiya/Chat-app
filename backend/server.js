f5ce30f3f7971fed241ba74f7ce668a7fa7bd1fa
Himanshu-Dodiya <himanshudodiya26@gmail.com>
2023-09-29 19:48:53 +0530

    Done UI of Login and Sign Up and db connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
-const app = express();
+const connectDB = require("./config/db");
+const colors =  require("colors");
 dotenv.config();
-
+connectDB();
+const app = express();
 app.get("/", (req, res) => {
   res.send("API is Running");
 });
@@ -19,4 +21,4 @@ app.get("/api/chat/:id", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, console.log(`Server started on PORT ${PORT}`));
+app.listen(5000, console.log(`Server started on PORT ${PORT}`.yellow.bold));
--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import Login from '../components/Authentication/Login'
+import Signup from '../components/Authentication/Signup'
 const HomePage = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m={"40px 0 15px 0"}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Text fontSize={"4xl"} fontFamily={"Work sans"} color={"black"}>
+          We-Chat
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} color="black" borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb='1em'>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>{<Login/>}</p>
+            </TabPanel>
+            <TabPanel>
+              <p><Signup/></p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
 export default HomePage;
